84316c81a8ef8ab5bedb86d5d77f163d099acdf1
weipu_glb <weipu.shan@gmail.com>
2020-05-22 02:52:57 +0200

    jdk help

---
+++ 00-basic/00/src/helloworld/Demo15_1API_Scanner.java
@@ -0,0 +1,29 @@
+package helloworld;
+
+/*
+API:    Application Programming Interface
+    1. 导包
+        import packageName.className;
+        // 如果在同一个包内，可省略导包语句
+        // java.lang 下的内容不需要导包，其他包需要import
+    2. 创建
+        className instance = new className(args);
+    3. 使用
+        instance.methodName(args);
+
+Class Scanner: 实现键盘输入数据
+    1. 导包
+        import packageName.className;
+        // 如果在同一个包内，可省略导包语句
+        // java.lang 下的内容不需要导包，其他包需要import
+    2. 创建
+        className instance = new className(args);
+    3. 使用
+        instance.methodName(args);
+
+ */
+
+public class Demo15_1API_Scanner {
+
+
+}
