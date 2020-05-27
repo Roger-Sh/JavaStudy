fd5c604b4466f7aa5929ccd30764e6c728f93e32
weipu_glb <weipu.shan@gmail.com>
2020-05-28 00:10:46 +0200

    String

--- 00-basic/00/src/helloworld/Demo17_2StringConst.java
+++ 00-basic/00/src/helloworld/Demo17_2StringConst.java
@@ -19,15 +19,25 @@ package helloworld;
 public class Demo17_2StringConst {
     public static void main(String[] args) {
 
-
+        // 通过字符串常量创建字符串， 字符串常量相同，字符串保存的地址值也相同
         String str1 = "abc";
         String str2 = "abc";
 
-        char[] charArray = {'a', 'b', 'c'};
-        String str3 = new String(charArray);
+        // 通过char[]创建字符串, 就算内容相同，但地址值不同，因为charArray并没有常量池
+        char[] charArray1 = {'a', 'b', 'c'};
+        String str3 = new String(charArray1);
+        char[] charArray2 = {'a', 'b', 'c'};
+        String str4 = new String(charArray2);
+
+        // 字符串常量池中，只要有一个字符不一样，也是两个不同的字符串常量，地址值不同
+        String str5 = "abcd";
+
 
         System.out.println(str1 == str2);   // true，  str1 与 str2 的地址值相同
         System.out.println(str1 == str3);   // false， str1 与 str3 地址值不同
         System.out.println(str2 == str3);   // false， str2 与 str3 地址值不同
+        System.out.println(str3 == str4);   // false， str3 与 str4 地址值不同
+        System.out.println(str1 == str5);   // false， str3 与 str4 地址值不同
+
     }
 }
