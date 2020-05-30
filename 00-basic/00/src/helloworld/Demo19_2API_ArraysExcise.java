00b87239bed6aec17a558680b056d2121c4a06f9
weipu_glb <weipu.shan@gmail.com>
2020-05-31 01:58:56 +0200

    Arrays

--- 00-basic/00/src/helloworld/Demo19_2API_ArraysExcise.java
+++ 00-basic/00/src/helloworld/Demo19_2API_ArraysExcise.java
@@ -1,4 +1,26 @@
 package helloworld;
 
+import java.util.Arrays;
+
 public class Demo19_2API_ArraysExcise {
+    public static void main(String[] args) {
+
+        /*
+            excise：将一个随机字符串中所有的字符升序排列，并倒序打印
+         */
+
+        // random string
+        String line = "kjlhasasdjkljALSKHFLAJSDlfskdjfLKJFSDLJ";
+
+        // convert String to char array
+        char[] charArray = line.toCharArray();
+
+        // ascend sort
+        Arrays.sort(charArray);
+
+        // reverse the array
+        for (int i = charArray.length - 1; i >= 0; i--) {
+            System.out.print(charArray[i] + " ");
+        }
+    }
 }
