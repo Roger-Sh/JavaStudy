f5d577e91b9bb2fee21e4eb51746f05a949927b3
weipu_glb <weipu.shan@gmail.com>
2020-06-14 14:11:31 +0200

    interface and implement

---
+++ 00-basic/00/src/basic_04_objectOrient/Demo24_1ClassPolymorphism.java
@@ -0,0 +1,14 @@
+package basic_04_objectOrient;
+
+/*
+面向对象三大特征：封装性Encapsulation，继承性Inheritance，多态性Polymorphism
+extends继承或者implements实现，是多态性的前提
+父类：人类
+子类：学生
+对象：小明，是一个学生，也是一个人类，这个对象拥有多种形态，这就是对象的多态性
+
+ */
+
+
+public class Demo24_1ClassPolymorphism {
+}
